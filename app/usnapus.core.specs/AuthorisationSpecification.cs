dc0520ef5f249c53b3ec7a588136689bd5943fe4
Owen Evans <owen@xero.com>
2013-03-03 21:45:30 +1300

    runnable on windows

---
+++ app/usnapus.core.specs/AuthorisationSpecification.cs
@@ -0,0 +1,35 @@
+﻿// ReSharper disable InconsistentNaming
+
+using System;
+using Machine.Fakes;
+using Machine.Specifications;
+
+namespace usnapus.core.specs
+{
+    [Subject(typeof(AuthorisationHandler))]
+    public abstract class AuthorisationSpecification:WithFakes
+    {
+        Establish context = () =>
+            {
+                _sut = new AuthorisationHandler(_webRequestFactory = An<IWebRequestFactory>());
+                _webRequestFactory.WhenToldTo(webRequestFactory => webRequestFactory.Create(Param.IsAny<Uri>())).Return(_webRequest);
+                
+            };
+
+        protected static AuthorisationHandler _sut;
+        protected static IWebRequestFactory _webRequestFactory;
+        protected static IWebRequest _webRequest;
+    }
+    public class when_registering_a_new_device:AuthorisationSpecification
+    {
+       public Because of = () => _token = _sut.AuthoriseDevice(_deviceId);
+        It should_return_a_token = () => _token.ShouldNotBeNull();
+        It should_call_device_register_url = () => _webRequestFactory.WasToldTo(webRequestFactory=>webRequestFactory.Create(Param<Uri>.Matches((p)=>p.Equals(_auth_uri))));
+        It should_be_a_post_call = () => _webRequest.Method.ShouldEqual("POST");
+        It should_set_credentials = () => _webRequest.Credentials.GetCredential(_auth_uri,"Digest").ShouldNotBeNull();
+        It should_set_password_to_device_id = () => _webRequest.Credentials.GetCredential(_auth_uri,"Digest").Password.ShouldEqual(_deviceId.ToString("N"));
+        static readonly Guid _deviceId = Guid.NewGuid();
+        static string _token;
+        static Uri _auth_uri = new Uri("https://api.usnap.us/device/register");
+    }
+}
---
+++ app/usnapus.core/AuthorisationHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+
+namespace usnapus.core
+{
+    public class AuthorisationHandler
+    {
+        readonly IWebRequestFactory _webRequestFactory;
+
+        public AuthorisationHandler(IWebRequestFactory webRequestFactory)
+        {
+            _webRequestFactory = webRequestFactory;
+        }
+
+        public string AuthoriseDevice(Guid deviceId)
+        {
+            var uri = new Uri("https://api.usnap.us/device/register");
+            var request = _webRequestFactory.Create(uri);
+            request.Method = "POST";
+            var credentialCache = new System.Net.CredentialCache();
+            credentialCache.add(uri,"Digest", new NetworkCredential("device",deviceId.ToString("N")));
+            request.Credentials = credentialCache;
+            return "";
+        }
+    }
+}
